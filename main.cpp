528a59be6075ac045d39a15a4e29158444b7f639
Milan Petrmichl <milan.petrmichl@gmx.de>
2025-08-05 23:58:49 +0200

    Klasse erweitert mit Initialisierungs Liste

--- main.cpp
+++ main.cpp
@@ -1,65 +1,58 @@
 #include <iostream>
 #include <utility>
 
-class Player1 {
+class Player {
 private:
-    char symbol = 'X';
+    char symbol;
 public:
-    static std::pair<int,int> player1Turn() {
+    Player(char s) : symbol(s) {};
+
+    std::pair<int,int> Turn() {
         std::pair<int,int> turn;
-        std::cout << "Player X: wo das Kreuz (row and column 0-2): ";
+        std::cout << "Player X: Wo setzen? (row and column 0-2): ";
         std::cin >> turn.first >> turn.second;
         return turn;
     }
 
-    [[nodiscard]] char getSymbol() const {
+    char getSymbol() const {
         return symbol;
     }
 
-
-    bool isWin(char table[3][3]) const{
-            for (int i = 0; i < 3; ++i) {
-                if (table[i][0] == symbol && table[i][1] == symbol && table[i][2] == symbol) return true;
-                if (table[0][i] == symbol && table[1][i] == symbol && table[2][i] == symbol) return true;
-            }
-            if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol) return true;
-            if (table[0][2] == symbol && table[1][1] == symbol && table[2][0] == symbol) return true;
-
-            return false;
-        }
-
-
 };
 
-class Player2 {
+/*class Player2 {
 private:
     char symbol = '0';
 public:
-    static std::pair<int,int> player2Turn() {
+    std::pair<int,int> player2Turn() {
         std::pair<int,int> turn;
-        std::cout << "Player 0: wo das Kreuz (row and column 0-2): ";
+        std::cout << "Player 0: Wo setzen? (row and column 0-2): ";
         std::cin >> turn.first >> turn.second;
         return turn;
     }
-    [[nodiscard]] char getSymbol() const{
+    char getSymbol() const{
         return symbol;
     }
 
-    bool isWin(char table[3][3]) const {
-        for (int i = 0; i < 3; ++i) {
-            if (table[i][0] == symbol && table[i][1] == symbol && table[i][2] == symbol) return true;
-            if (table[0][i] == symbol && table[1][i] == symbol && table[2][i] == symbol) return true;
-        }
-        if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol) return true;
-        if (table[0][2] == symbol && table[1][1] == symbol && table[2][0] == symbol) return true;
+};*/
 
-        return false;
+
+
+
+
+bool isWin(char table[3][3], char symbol)  {
+    for (int i = 0; i < 3; ++i) {
+        if (table[i][0] == symbol && table[i][1] == symbol && table[i][2] == symbol) return true;
+        if (table[0][i] == symbol && table[1][i] == symbol && table[2][i] == symbol) return true;
     }
+    if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol) return true;
+    if (table[0][2] == symbol && table[1][1] == symbol && table[2][0] == symbol) return true;
 
-};
+    return false;
+}
 
 
-void printTable(char table[3][3], Player1& p1, Player2& p2) {
+void printTable(char table[3][3], Player& p1, Player& p2) {
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             if (table[i][j] == ' '){
@@ -77,9 +70,12 @@ void printTable(char table[3][3], Player1& p1, Player2& p2) {
 }
 
 
+
+
+
 int main() {
-    Player1 p1;
-    Player2 p2;
+    Player player1('X');
+    Player player2('0');
     char table[3][3];
     int turncount = 0;
 
@@ -88,12 +84,11 @@ int main() {
         for (int j = 0; j < 3; ++j)
             table[i][j] = ' ';
 
-    printTable(table,p1,p2);
+    printTable(table,player1,player2);
 
 
     while (true) {
-
-        std::pair<int, int> MyTurn = Player1::player1Turn();
+        std::pair<int, int> MyTurn = player1.Turn();
 
         if (MyTurn.first < 0 || MyTurn.first > 2 || MyTurn.second < 0 || MyTurn.second > 2) {
             std::cout << "Invalid position. Try again.\n";
@@ -105,20 +100,20 @@ int main() {
             continue;
         }
         turncount++;
-        table[MyTurn.first][MyTurn.second] = p1.getSymbol();
-        printTable(table, p1, p2);
+        table[MyTurn.first][MyTurn.second] = player1.getSymbol();
+        printTable(table, player1, player2);
 
+        if (isWin(table, player1.getSymbol())) {
+            std::cout << "Player " << player1.getSymbol() << " wins!\n";
+            break;
+        }
         if (turncount == 9){
             std::cout << "unentschieden!" << std::endl;
             return 1;
         }
 
-        if (p1.isWin(table)) {
-            std::cout << "Player " << p1.getSymbol() << " wins!\n";
-            break;
-        }
+        std::pair<int, int> PlayerTurn = player2.Turn();
 
-        std::pair<int, int> PlayerTurn = Player2::player2Turn();
 
         if (PlayerTurn.first < 0 || PlayerTurn.first > 2 || PlayerTurn.second < 0 || PlayerTurn.second > 2) {
             std::cout << "Invalid position. Try again.\n";
@@ -130,17 +125,17 @@ int main() {
             continue;
         }
         turncount++;
-        table[PlayerTurn.first][PlayerTurn.second] = p2.getSymbol();
-        printTable(table, p1, p2);
+        table[PlayerTurn.first][PlayerTurn.second] = player2.getSymbol();
+        printTable(table, player1, player2);
 
-        if (turncount == 9){
-            std::cout << "unentschieden!" << std::endl;
-            return 1;
-        }
-
-            if (p2.isWin(table)){
-                std::cout << "Player " << p2.getSymbol() << " wins!\n";
+            if (isWin(table,player2.getSymbol())){
+                std::cout << "Player " << player2.getSymbol() << " wins!\n";
                 break;
+            }
+
+            if (turncount == 9){
+                std::cout << "\nunentschieden!" << std::endl;
+                return 1;
             }
 
     }
