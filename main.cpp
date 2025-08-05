b97be96d3b1f5a0249268931caac1b0f36f76694
Milan Petrmichl <milan.petrmichl@gmx.de>
2025-08-05 15:06:38 +0200

    Second commit. Current State

--- main.cpp
+++ main.cpp
@@ -12,9 +12,22 @@ public:
         return turn;
     }
 
-    [[nodiscard]] char getSymbol() const{
+    [[nodiscard]] char getSymbol() const {
         return symbol;
     }
+
+
+    bool isWin(char table[3][3]) const{
+            for (int i = 0; i < 3; ++i) {
+                if (table[i][0] == symbol && table[i][1] == symbol && table[i][2] == symbol) return true;
+                if (table[0][i] == symbol && table[1][i] == symbol && table[2][i] == symbol) return true;
+            }
+            if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol) return true;
+            if (table[0][2] == symbol && table[1][1] == symbol && table[2][0] == symbol) return true;
+
+            return false;
+        }
+
 
 };
 
@@ -32,10 +45,21 @@ public:
         return symbol;
     }
 
+    bool isWin(char table[3][3]) const {
+        for (int i = 0; i < 3; ++i) {
+            if (table[i][0] == symbol && table[i][1] == symbol && table[i][2] == symbol) return true;
+            if (table[0][i] == symbol && table[1][i] == symbol && table[2][i] == symbol) return true;
+        }
+        if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol) return true;
+        if (table[0][2] == symbol && table[1][1] == symbol && table[2][0] == symbol) return true;
+
+        return false;
+    }
+
 };
 
 
-void printTable(char table[3][3], Player1 p1, Player2 p2) {
+void printTable(char table[3][3], Player1& p1, Player2& p2) {
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             if (table[i][j] == ' '){
@@ -52,32 +76,12 @@ void printTable(char table[3][3], Player1 p1, Player2 p2) {
     }
 }
 
-bool isWin(char table[3][3], Player1& p1){
-    for (int i = 0; i < 3; ++i) {
-        if (table[i][0] == p1.getSymbol() && table[i][1] == p1.getSymbol() && table[i][2] == p1.getSymbol()) return true;
-        if (table[0][i] == p1.getSymbol() && table[1][i] == p1.getSymbol() && table[2][i] == p1.getSymbol()) return true;
-    }
-    if (table[0][0] == p1.getSymbol() && table[1][1] == p1.getSymbol() && table[2][2] == p1.getSymbol()) return true;
-    if (table[0][2] == p1.getSymbol() && table[1][1] == p1.getSymbol() && table[2][0] == p1.getSymbol()) return true;
-
-    return false;
-}
-
-bool isWin2(char table[3][3], Player2& p2){
-    for (int i = 0; i < 3; ++i) {
-        if (table[i][0] == p2.getSymbol() && table[i][1] == p2.getSymbol() && table[i][2] == p2.getSymbol()) return true;
-        if (table[0][i] == p2.getSymbol() && table[1][i] == p2.getSymbol() && table[2][i] == p2.getSymbol()) return true;
-    }
-    if (table[0][0] == p2.getSymbol() && table[1][1] == p2.getSymbol() && table[2][2] == p2.getSymbol()) return true;
-    if (table[0][2] == p2.getSymbol() && table[1][1] == p2.getSymbol() && table[2][0] == p2.getSymbol()) return true;
-
-    return false;
-}
 
 int main() {
     Player1 p1;
     Player2 p2;
     char table[3][3];
+    //int turncount = 0;
 
     // Initialize table
     for (int i = 0; i < 3; ++i)
@@ -87,6 +91,7 @@ int main() {
     printTable(table,p1,p2);
 
     while (true) {
+
         std::pair<int,int> MyTurn = Player1::player1Turn();
 
         if (MyTurn.first < 0 || MyTurn.first > 2 || MyTurn.second < 0 || MyTurn.second > 2) {
@@ -99,23 +104,35 @@ int main() {
             continue;
         }
 
-        table[MyTurn.first][MyTurn.second] = p1.getSymbol();
-        printTable(table,p1,p2);
+            table[MyTurn.first][MyTurn.second] = p1.getSymbol();
+            printTable(table, p1, p2);
 
-        std::pair<int, int> PlayerTurn = Player2::player2Turn();
+            if (p1.isWin(table)){
+                std::cout << "Player " << p1.getSymbol() << " wins!\n";
+                break;
+            }
 
-        table[PlayerTurn.first][PlayerTurn.second] = p2.getSymbol();
-        printTable(table,p1,p2);
+            std::pair<int, int> PlayerTurn = Player2::player2Turn();
 
-        if (isWin(table, p1)){
-            std::cout << "Player with " << p1.getSymbol() << " wins!\n";
-            break;
-        }
-        else if (isWin2(table,p2)){
-            std::cout << "Player with " << p2.getSymbol() << " wins!\n";
-            break;
-        }
+            if (PlayerTurn.first < 0 || PlayerTurn.first > 2 || PlayerTurn.second < 0 || PlayerTurn.second > 2) {
+            std::cout << "Invalid position. Try again.\n";
+            continue;
+                }
+
+            if (table[PlayerTurn.first][PlayerTurn.second]  != ' ') {
+            std::cout << "Cell already taken. Try again.\n";
+            continue;
+                }
+
+            table[PlayerTurn.first][PlayerTurn.second] = p2.getSymbol();
+            printTable(table, p1, p2);
+
+
+            if (p2.isWin(table)){
+                std::cout << "Player " << p2.getSymbol() << " wins!\n";
+                break;
+            }
+
     }
-
     return 0;
 }
